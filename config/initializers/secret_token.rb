8b06087c8fdd4c801db1dbe43076e7a2e05183cd
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-04-19 21:17:46 +0200

    create new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MealplannerNew::Application.config.secret_token = 'fe28925d9653c1d70684d3953dc8110265a1f1193cf0954299045522ceb945deab0fddc42374baf8722dfde9c8a6f7e516c6dd780a8225c3e5638295be19f943'
