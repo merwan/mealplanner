631dff650173ef038dd91c46c45f4bda3e56aae4
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-04-25 21:32:48 +0200

    add basic server side validation. Fixes #8

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,12 @@
 class Meal < ActiveRecord::Base
   attr_accessible :eating_day, :menu_description
+  validate :eating_day_is_date?
+
+  private
+
+  def eating_day_is_date?
+    if !eating_day.is_a?(Date)
+      errors.add(:eating_day, 'must be a valid date')
+    end
+  end
 end
